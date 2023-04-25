7562f01dd8fcd018a6a9d643471553efd51ef364
kesraoui mohamed <kesraoui15003@gmail.com>
2023-04-25 13:11:39 +0200

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const h = document.querySelector("h1");
-const aray = [
-    
-]
 
-let rand= math.floor(math.rand*Aray.length)
+let interval = null;
+
+h.addEventListener("mouseover", function (event) {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    const randomLetters = event.target.innerText.split("").map((letter, index) => {
+      if (index < iteration) {
+        return event.target.dataset.value[index];
+      }
+
+      return letters[Math.floor(Math.random() * 26)];
+    });
+
+    event.target.innerText = randomLetters.join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 60);
+});
